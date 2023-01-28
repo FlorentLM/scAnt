a749a9aa70a7967118f6b3fe891c016bd1b8138f
Florent Le Moël <25004801+FlorentLM@users.noreply.github.com>
2023-01-28 02:02:19 +0000

    Fixes for 'spawn' process start method

--- temporary_stuff/post_processing.py
+++ temporary_stuff/post_processing.py
@@ -4,11 +4,61 @@ import cv2
 import numpy as np
 from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 import time
-import os
 from natsort import natsorted, ns
 import platform
 import shutil
 import subprocess
+from itertools import repeat
+
+
+def get_stacks_names(raw_folder_path):
+    """
+    Returns a naturally sorted list of all stacks names present in the RAW folder
+    """
+    raw_folder_path = Path(raw_folder_path)
+    uniq_names = set([img_path.stem.split('step')[0] for img_path in raw_folder_path.glob("*.tif")])
+    list_stacks_names = list(uniq_names)
+    return natsorted(list_stacks_names, alg=ns.IGNORECASE)
+
+
+def get_paths_by_stack(stacks_names):
+    """
+    Returns all the raw images paths, grouped by stack (in a list of lists)
+    """
+    all_stacks = []
+    for stack in stacks_names:
+        images_current_stack = list(raw_images_folder.glob(f"{stack}*.tif"))
+        all_stacks.append(natsorted(images_current_stack, alg=ns.IGNORECASE))
+    return all_stacks
+
+
+def lookup_external_executable(bin_name):
+    """
+    On macOS and Linux, fetches the absolute path for an executable. On Windows, it tries first the PATH and
+    then looks it up in the local project folder.
+    """
+    # TODO: add pre-compiled binaries for Linux and macOS like for Windows?
+
+    if platform.system().lower().startswith('win'):
+        # Check if the executable is in the PATH
+        which_res = shutil.which(f"{bin_name}.exe")
+
+        if which_res is not None:
+            path_executable = Path(which_res)
+
+        else:
+            cwd = Path().cwd()
+
+            local_build = cwd / 'external'
+            if not local_build.exists():
+                local_build = cwd.parent / 'external'
+
+            path_executable = Path(local_build) / f"{bin_name}.exe"
+
+    else:
+        path_executable = Path(shutil.which("bin_name"))
+
+    return path_executable
 
 
 def measure_focus(image, display=False):
@@ -46,9 +96,11 @@ def neutral_grayscale(colour_image):
     return mono
 
 
-def focus_check_worker(image_path):
-
-    if VERBOSE:
+def focus_check_single(image_path, threshold, display, verbose):
+    """
+    Performs the focus check on a single image and decides if it is blurry or not, based on the passed threshold value
+    """
+    if verbose:
         print(f"Thread {str(threading.get_ident())[-5:]} (PID {threading.get_native_id()}) processing {image_path.stem}{image_path.suffix}")
 
     image = cv2.imread(image_path.as_posix())
@@ -62,10 +114,10 @@ def focus_check_worker(image_path):
     # gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)      # isn't the human-perception-centered algorithm too biased?
     gray = neutral_grayscale(resized)
 
-    focus_score = measure_focus(gray, display=DISPLAY)
+    focus_score = measure_focus(gray, display=display)
 
     # If the focus measure is less than the supplied threshold, then the image should be considered "blurry"
-    if focus_score < FOCUS_THRESH:
+    if focus_score < threshold:
         text = "BLURRY"
         col = (0, 0, 255)
         is_focused = False
@@ -75,10 +127,10 @@ def focus_check_worker(image_path):
         col = (255, 0, 0)
         is_focused = True
 
-    if VERBOSE:
+    if verbose:
         print(f"{image_path.stem}{image_path.suffix} is {text} (score: {focus_score:.2f})")
 
-    if DISPLAY:
+    if display:
         cv2.putText(resized, f"{text}: {focus_score:.2f}", (10, 30), cv2.FONT_HERSHEY_DUPLEX, 0.8, col, 3)
         cv2.imshow("Image", resized)
         cv2.waitKey(1)
@@ -86,10 +138,16 @@ def focus_check_worker(image_path):
     return image_path, is_focused, focus_score
 
 
-def multiprocessed_focus_check(paths_list):
-
+def focus_check_multi(paths_list, threshold, display=False, verbose=False):
+    """
+    Performs the focus check on a list of images, and sorts them into two lists: sharp and blurry
+    """
     thread_executor = ThreadPoolExecutor()
-    thread_results = thread_executor.map(focus_check_worker, paths_list)
+    thread_results = thread_executor.map(focus_check_single,
+                                         paths_list,
+                                         repeat(threshold),
+                                         repeat(display),
+                                         repeat(verbose))
 
     sharp = []
     blurry = []
@@ -104,7 +162,6 @@ def multiprocessed_focus_check(paths_list):
             blurry.append(image)
 
     sharp = natsorted(sharp, alg=ns.IGNORECASE)
-
     # sharp = [x for _, x in sorted(zip(focused_scores, sharp))]
     # sharp.reverse()
     blurry = natsorted(blurry, alg=ns.IGNORECASE)
@@ -112,63 +169,17 @@ def multiprocessed_focus_check(paths_list):
     return sharp, blurry
 
 
-def get_stacks_names(raw_folder_path):
+def alignment(images_paths, output_folder):
     """
-    Returns a naturally sorted list of all stacks names present in the RAW folder
+    Aligns a list of images using Hugin, and saves the files to disk
     """
-    raw_folder_path = Path(raw_folder_path)
-    uniq_names = set([img_path.stem.split('step')[0] for img_path in raw_folder_path.glob("*.tif")])
-    list_stacks_names = list(uniq_names)
-    return natsorted(list_stacks_names, alg=ns.IGNORECASE)
-
-
-def get_paths_by_stack(stacks_names):
-    """
-    Returns all the raw images paths, grouped by stack (in a list of lists)
-    """
-    all_stacks = []
-    for stack in stacks_names:
-        images_current_stack = list(raw_images_folder.glob(f"{stack}*.tif"))
-        all_stacks.append(natsorted(images_current_stack, alg=ns.IGNORECASE))
-    return all_stacks
-
-
-def lookup_external_executables():
-
-    if platform.system().lower().startswith('win'):
-        # Check if the executables are in the PATH
-        which_hugin = shutil.which("align_image_stack.exe")
-        which_enfuse = shutil.which("enfuse.exe")
-
-        if which_hugin is not None and which_enfuse is not None:
-            path_hugin = Path(which_hugin)
-            path_enfuse = Path(which_enfuse)
-
-        else:
-            cwd = Path().cwd()
-
-            local_build = cwd / 'external'
-            if not local_build.exists():
-                local_build = cwd.parent / 'external'
-
-            path_hugin = Path(local_build) / "align_image_stack.exe"
-            path_enfuse = Path(local_build) / "enfuse.exe"
-
-    else:
-        path_hugin = Path(shutil.which("align_image_stack"))
-        path_enfuse = Path(shutil.which("enfuse"))
-
-    return path_hugin, path_enfuse
-
-
-def multiprocessed_alignment(images_paths):
-
-    global aligned_images_folder
-
     prefix = images_paths[0].stem.split('step')[0]
     inputs = [p.as_posix() for p in images_paths]
 
-    subprocess.run([HUGIN_PATH.as_posix(),
+    output_folder = Path(output_folder)
+    hugin_path = lookup_external_executable('align_image_stack')
+
+    subprocess.run([hugin_path.as_posix(),
                     "-v",
                     "-m",
                     "-x",
@@ -180,17 +191,20 @@ def multiprocessed_alignment(images_paths):
                     f"-a {prefix}_ALIGNED",
                     *inputs
                     ],
-                   cwd=aligned_images_folder)
+                   cwd=output_folder)
 
 
-def multiprocessed_fuse(images_paths):
-
-    global stacked_images_folder
-
+def fuse(images_paths, output_folder):
+    """
+    Fuses multiple images of different focus into a focus stack, using Hugin's enfuse
+    """
     inputs = [p.as_posix() for p in images_paths]
     inputs.reverse()
 
-    subprocess.run([ENFUSE_PATH.as_posix(),
+    output_folder = Path(output_folder)
+    enfuse_path = lookup_external_executable('enfuse')
+
+    subprocess.run([enfuse_path.as_posix(),
                     " --exposure-weight=0",
                     " --saturation-weight=0",
                     " --contrast-weight=1",
@@ -198,75 +212,87 @@ def multiprocessed_fuse(images_paths):
                     " --contrast-edge-scale=1",
                     # "--save-masks",               # to save soft and hard masks
                     # "--gray-projector=l-star",    # alternative stacking method
-                    f" --output={stacked_images_folder.as_posix()}",
+                    f" --output={output_folder.as_posix()}",
                     *inputs
                     ],
-                   cwd=stacked_images_folder)
+                   cwd=output_folder)
 
 
 ########
 
-raw_images_folder = Path("/home/florent/Desktop/atta_vollenweideri_000139_2/test_project/RAW")
-
 DISPLAY = False
 VERBOSE = False
-
 FOCUS_THRESH = 5
 
-NUM_CPU_CORES = os.cpu_count()
 
-HUGIN_PATH, ENFUSE_PATH = lookup_external_executables()
+if __name__ == '__main__':
 
-##
+    raw_images_folder = Path("/Users/florent/Desktop/atta_vollenweideri_000139_2/test_project/RAW")
 
-stacks_names = get_stacks_names(raw_images_folder)
-paths_by_stack = get_paths_by_stack(stacks_names)
+    stacks_names = get_stacks_names(raw_images_folder)
+    paths_by_stack = get_paths_by_stack(stacks_names)
 
-##
+    ##
 
-start = time.time()
+    print('Checking focus...')
+    start = time.time()
 
-with ProcessPoolExecutor() as executor:
-    focus_proc_results = executor.map(multiprocessed_focus_check, paths_by_stack)
+    with ProcessPoolExecutor() as executor:
 
-end = time.time()
+        # Note:
+        # It is necessary to explicitly pass the global args FOCUS_THRESH, DISPLAY and VERBOSE to the map() call here,
+        # despite them being common to each child process (unlike the image path values) - this is due to the fact
+        # that the 'spawn' start method (see multiprocessing.get_context()) does not pass the global-level variables
+        # to the child processes.
+        # With the 'fork' start method (the default on Linux), the child process *do* inherit the globals, and it is
+        # thus not necessary to pass them to map() - only the iterable containing the image paths is needed.
+        # However, the 'fork' start method is unsafe on macOS, and does not exist on Windows.
 
-focus_results = list(focus_proc_results)
+        focus_proc_results = executor.map(focus_check_multi,
+                                          paths_by_stack,          # 1 value of the iterable for 1 child process
+                                          repeat(FOCUS_THRESH),    # the arg is repeated, same for each child process
+                                          repeat(DISPLAY),
+                                          repeat(VERBOSE))
+    end = time.time()
 
-for stack_name, stack_result in zip(stacks_names, focus_results):
-    sharp_images, blurry_images = stack_result
-    print(f"Found {len(sharp_images)} focused images and {len(blurry_images)} blurry images for stack {stack_name}.")
+    focus_results = list(focus_proc_results)
 
-print(f"Focus check took {end - start} seconds.")
+    for stack_name, stack_result in zip(stacks_names, focus_results):
+        sharp_images, blurry_images = stack_result
+        print(f"Found {len(sharp_images)} focused images and {len(blurry_images)} blurry images for stack {stack_name}.")
 
-##
+    print(f"Focus check took {end - start} seconds.")
 
-aligned_images_folder = raw_images_folder.parent / 'aligned'
-aligned_images_folder.mkdir(parents=True, exist_ok=True)
+    ##
 
-##
+    aligned_images_folder = raw_images_folder.parent / 'aligned'
+    aligned_images_folder.mkdir(parents=True, exist_ok=True)
 
-only_focused_by_stack = [group[0] for group in focus_results]
+    only_focused_by_stack = [group[0] for group in focus_results]
 
-start = time.time()
+    print('Aligning...')
+    start = time.time()
 
-with ProcessPoolExecutor() as executor:
-    executor.map(multiprocessed_alignment, only_focused_by_stack)
+    with ProcessPoolExecutor() as executor:
+        executor.map(alignment, only_focused_by_stack,
+                     repeat(aligned_images_folder))
 
-end = time.time()
+    end = time.time()
 
-print(f"Alignment took {end - start} seconds.")
+    print(f"Alignment took {end - start} seconds.")
 
-##
+    ##
 
-stacked_images_folder = raw_images_folder.parent / 'stacked'
-stacked_images_folder.mkdir(parents=True, exist_ok=True)
+    stacked_images_folder = raw_images_folder.parent / 'stacked'
+    stacked_images_folder.mkdir(parents=True, exist_ok=True)
 
-start = time.time()
+    print('Fusing...')
+    start = time.time()
 
-with ProcessPoolExecutor() as executor:
-    executor.map(multiprocessed_fuse, paths_by_stack)
+    with ProcessPoolExecutor() as executor:
+        executor.map(fuse, paths_by_stack,
+                     repeat(stacked_images_folder))
 
-end = time.time()
+    end = time.time()
 
-print(f"Fuse took {end - start} seconds.")
+    print(f"Fuse took {end - start} seconds.")
